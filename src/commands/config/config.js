3c0723572a402b6d8d6cf4da8d3e0ab2c2d8f763
C0de pc <boltlucas05@gmail.com>
2022-11-28 16:40:28 -0300

    make it better

--- src/commands/config/config.js
+++ src/commands/config/config.js
@@ -1,55 +1,62 @@
 const Command = require('../../structures/Command')
 
 module.exports = class extends Command {
-  constructor(client) {
-    super(client, {
-      name: 'config',
-      description: 'Configuração do bot',
-      requireDatabase: true,
-      options:[
-        {
-          type: 'SUB_COMMAND_GROUP',
-          name: 'welcome',
-          description: 'Configuração de boas vindas',
-          options: [
-            {
-              type: 'SUB_COMMAND',
-              name: 'canal_entrada',
-              description: 'configurar o canal onde será enviada',
-              options: [
+    constructor(client) {
+        super(client, {
+            name: 'config',
+            description: 'Configurar dados do servidor no bot.',
+            requireDatabase: true,
+            options: [
                 {
-                  type: 'CHANNEL',
-                  name: 'canal',
-                  description: 'canal de texto onde será enviada!',
-                  required: true
-                }
-              ]
-            },
-            {
-              type: 'SUB_COMMAND',
-              name: 'mensagem_entrada',
-              description: 'Configurar a mensagem que sera enviada',
-              options: [
+                    type: 'SUB_COMMAND_GROUP',
+                    name: 'welcome',
+                    description: 'Configuração do sistema de boas-vindas.',
+                    options: [
+                        {
+                            type: 'SUB_COMMAND',
+                            name: 'canal_entrada',
+                            description: 'Configurar o canal onde a mensagem de boas-vindas será enviada.',
+                            options: [
+                                {
+                                    type: 'CHANNEL',
+                                    name: 'canal',
+                                    description: 'Canal de texto onde a mensagem será enviada.',
+                                    required: true
+                                }
+                            ]
+                        }
+                    ]
+                },
                 {
-                  type: 'STRING',
-                  name: 'mensagem',
-                  description: 'Mensagem que sera enviada',
-                  required: true
+                    type: 'SUB_COMMAND_GROUP',
+                    name: 'ticket',
+                    description: 'Configuração do sistema de tickets.',
+                    options: [
+                        {
+                            type: 'SUB_COMMAND',
+                            name: 'mensagem',
+                            description: 'Mensagem onde o usuário irá clicar para abrir o ticket.',
+                            options: [
+                                {
+                                    type: 'CHANNEL',
+                                    name: 'canal',
+                                    description: 'Canal de texto onde a mensagem será enviada.',
+                                    required: true
+                                }
+                            ]
+                        }
+                    ]
                 }
-              ]
-            }
-          ]
-        }
-      ]
-    })
-  }
+            ]
+        })
+    }
 
-  run = (interaction) => {
-  if (!interaction.member.permissions.has('MANAGE_GUILD')) return interaction.reply({ content: 'Você não tem permissão para utilizar este comando!', ephemeral: true })
+    run = (interaction) => {
+        if (!interaction.member.permissions.has('MANAGE_GUILD')) return interaction.reply({ content: 'Você não tem permissão para utilizar este comando!', ephemeral: true })
 
         const subCommandGroup = interaction.options.getSubcommandGroup()
         const subCommand = interaction.options.getSubcommand()
 
         require(`../../subCommands/config/${subCommandGroup}/${subCommand}`)(this.client, interaction)
-  }
+    }
 }
---
+++ src/util/ticketCategories.js
@@ -0,0 +1,26 @@
+const { MessageButton, MessageEmbed } = require('discord.js')
+/*
+    {
+        button: MessageButton,
+        embed: MessageEmbed,
+        name: String,
+        id: String
+    }
+*/
+
+module.exports = [
+    {
+        button: new MessageButton().setCustomId(`openTicket-1046833233154818129`).setLabel(`Suporte do bot`).setEmoji('🛒').setStyle('PRIMARY'),
+        embed: new MessageEmbed().setDescription('Bem vindo ao setor de compras'),
+        name: 'suportebot',
+        id: '1046833233154818129',
+        staffRole: '1046621307980234852'
+    },
+    {
+        button: new MessageButton().setCustomId(`openTicket-1046834458034192484`).setLabel(`Suporte do servidor`).setEmoji('📞').setStyle('PRIMARY'),
+        embed: new MessageEmbed().setDescription('Bem vindo ao suporte ao suporte do servidor'),
+        name: 'suporteservidor',
+        id: '1046834458034192484',
+        staffRole: '1046621307980234852'
+    }
+]
