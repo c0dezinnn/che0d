9b4cb922f303e487f38e5c0c81673e61994381af
C0dezinnn <boltlucas05@gmail.com>
2022-12-27 22:41:13 -0300

    joya

--- src/commands/diversão/joya.js
+++ src/commands/diversão/joya.js
@@ -1,5 +1,6 @@
 const Command = require('../../structures/Command')
 const {MessageActionRow, MessageButton} = require('discord.js')
+const fs = require('fs')
 const row = new MessageActionRow()
     .addComponents(
         [
@@ -15,12 +16,16 @@ module.exports = class extends Command {
     constructor(client) {
         super(client, {
             name: 'joya',
-            description: 'The king joya',
+            description: 'the king joya',
             options: []
         })
     }
 
     run = async (interaction) => {
-        
+        const wait = require('util').promisify(setTimeout);
+        const joya = fs.readFileSync(`./imgs/joya.png`)
+        await interaction.deferReply();
+        await wait(2000);
+        interaction.editReply({files: [joya]})
     }
 	}
